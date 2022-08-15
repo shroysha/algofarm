5650dcf9837be519144c07cb282dfd1aa45e0df3
Shawn Shroyer <shroysha@gmail.com>
2022-08-15 05:02:29 -0400

    converting to unit

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ export const fetchSignature = async () => {
   const response = await fetch('api/harvest', {
     method: 'POST',
   });
-  const res = (await response.json()) as HarvestResponse;
-  return res.signature;
+  const res = (await response.json()) as any;
+
+  return Uint8Array.from(res.signature);
 };

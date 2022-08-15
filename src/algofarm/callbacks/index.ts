66f14eae9aa33cefd1c9384ad3099d2426fd0388
Shawn Shroyer <shroysha@gmail.com>
2022-08-15 05:07:20 -0400

    opt in

--- src/algofarm/callbacks/index.ts
+++ src/algofarm/callbacks/index.ts
@@ -28,12 +28,20 @@ export const useOptInCallback = (accountAddress: string | null) => {
       amount: 0,
       suggestedParams,
     });
+    const opt3 = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: accountAddress,
+      to: accountAddress,
+      assetIndex: minterId,
+      amount: 0,
+      suggestedParams,
+    });
 
-    algosdk.assignGroupID([opt1, opt2]);
+    algosdk.assignGroupID([opt1, opt2, opt3]);
 
     const multipleTxnGroups = [
       { txn: opt1, signers: [accountAddress] },
       { txn: opt2, signers: [accountAddress] },
+      { txn: opt3, signers: [accountAddress] },
     ];
 
     await signAndSend(multipleTxnGroups, algod);

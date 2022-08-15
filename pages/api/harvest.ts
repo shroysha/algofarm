d548aa8669b08b85a7d900e2f9ae6fff0970d489
Shawn Shroyer <shroysha@gmail.com>
2022-08-15 10:36:21 -0400

    fixing signature

--- pages/api/harvest.ts
+++ pages/api/harvest.ts
@@ -2,7 +2,9 @@ import algosdk, { decodeAddress } from 'algosdk';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { minterContract, nft1 } from '../../src/constants';
 import { HarvestBody, HarvestResponse } from '../../src/types';
-import crypto from 'crypto';
+import { getPrivateKey, getPublicKey } from '../../src/util';
+import * as ed from '@noble/ed25519';
+
 // http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
 
 /* utf.js - UTF-8 <=> UTF-16 convertion
@@ -13,63 +15,13 @@ import crypto from 'crypto';
  * This library is free.  You can redistribute it and/or modify it.
  */
 
-function Utf8ArrayToStr(array: any) {
-  var out, i, len, c;
-  var char2, char3;
-
-  out = '';
-  len = array.length;
-  i = 0;
-  while (i < len) {
-    c = array[i++];
-    switch (c >> 4) {
-      case 0:
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-        // 0xxxxxxx
-        out += String.fromCharCode(c);
-        break;
-      case 12:
-      case 13:
-        // 110x xxxx   10xx xxxx
-        char2 = array[i++];
-        out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
-        break;
-      case 14:
-        // 1110 xxxx  10xx xxxx  10xx xxxx
-        char2 = array[i++];
-        char3 = array[i++];
-        out += String.fromCharCode(
-          ((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0)
-        );
-        break;
-    }
-  }
-
-  return out;
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<HarvestResponse>
 ) {
   const {} = req.body as HarvestBody;
-  const monic = process.env.MONIC;
-  if (monic == null) {
-    throw new Error('Monic not defined');
-  }
-  console.log(monic);
-  const sk = algosdk.mnemonicToSecretKey(monic).sk;
-  console.log();
+
   const nonce = 0;
-  const signer = decodeAddress(
-    'FDKUZJVGIFUAYSJIH7CIQMDV4MH6ZLBHEXFHFENWDS3RRZPGVTWFLQZMLU'
-  ).publicKey;
 
   const messageInts: number[] = [];
   for (let x of new Uint8Array(Buffer.from('ProgData'))) {
@@ -84,15 +36,10 @@ export default function handler(
     messageInts.push(x);
   }
 
-  const toSign = new Uint8Array(messageInts);
-  const signature = algosdk.signBytes(toSign, sk);
+  const message = new Uint8Array(messageInts);
+  const signature = await ed.sign(message, getPrivateKey());
+  const isValid = await ed.verify(signature, message, getPublicKey());
 
-  const signatureLength = signature.length;
-
-  console.log({
-    toSign: Utf8ArrayToStr(toSign),
-    signature: Utf8ArrayToStr(signature),
-    signer: Utf8ArrayToStr(signer),
-  });
-  res.status(200).json({ signature, signatureLength });
+  console.log({ message, signature, isValid });
+  res.status(200).json({ signature });
 }
--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,4 @@ export type HarvestBody = {};
 
 export type HarvestResponse = {
   signature: Uint8Array;
-  signatureLength: number;
 };

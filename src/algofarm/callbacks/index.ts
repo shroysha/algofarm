ecafb1733202c947026e680a495e5d33f112f4db
Shawn Shroyer <shroysha@gmail.com>
2022-08-16 08:40:53 -0400

    adding

--- src/algofarm/callbacks/index.ts
+++ src/algofarm/callbacks/index.ts
@@ -97,42 +97,42 @@ export const useDoBurnCallback = (accountAddress: string | null) => {
       amount: 1,
       suggestedParams,
     });
-    // const opt1 = makeNothing(accountAddress, suggestedParams, 0);
-    // const opt2 = makeNothing(accountAddress, suggestedParams, 1);
-    // const opt3 = makeNothing(accountAddress, suggestedParams, 2);
-    // const opt4 = makeNothing(accountAddress, suggestedParams, 3);
-    // const opt5 = makeNothing(accountAddress, suggestedParams, 4);
-    // const opt6 = makeNothing(accountAddress, suggestedParams, 5);
-    // const opt7 = makeNothing(accountAddress, suggestedParams, 6);
-    // const opt8 = makeNothing(accountAddress, suggestedParams, 7);
-    // const opt9 = makeNothing(accountAddress, suggestedParams, 8);
+    const opt1 = makeNothing(accountAddress, suggestedParams, 0);
+    const opt2 = makeNothing(accountAddress, suggestedParams, 1);
+    const opt3 = makeNothing(accountAddress, suggestedParams, 2);
+    const opt4 = makeNothing(accountAddress, suggestedParams, 3);
+    const opt5 = makeNothing(accountAddress, suggestedParams, 4);
+    const opt6 = makeNothing(accountAddress, suggestedParams, 5);
+    const opt7 = makeNothing(accountAddress, suggestedParams, 6);
+    const opt8 = makeNothing(accountAddress, suggestedParams, 7);
+    const opt9 = makeNothing(accountAddress, suggestedParams, 8);
 
     algosdk.assignGroupID([
       opContract,
       burnTxn,
       opt1,
-      // opt2,
-      // opt3,
-      // opt4,
-      // opt5,
-      // opt6,
-      // opt7,
-      // opt8,
-      // opt9,
+      opt2,
+      opt3,
+      opt4,
+      opt5,
+      opt6,
+      opt7,
+      opt8,
+      opt9,
     ]);
 
     const multipleTxnGroups = [
       { txn: opContract, signers: [accountAddress] },
       { txn: burnTxn, signers: [accountAddress] },
       { txn: opt1, signers: [accountAddress] },
-      // { txn: opt2, signers: [accountAddress] },
-      // { txn: opt3, signers: [accountAddress] },
-      // { txn: opt4, signers: [accountAddress] },
-      // { txn: opt5, signers: [accountAddress] },
-      // { txn: opt6, signers: [accountAddress] },
-      // { txn: opt7, signers: [accountAddress] },
-      // { txn: opt8, signers: [accountAddress] },
-      // { txn: opt9, signers: [accountAddress] },
+      { txn: opt2, signers: [accountAddress] },
+      { txn: opt3, signers: [accountAddress] },
+      { txn: opt4, signers: [accountAddress] },
+      { txn: opt5, signers: [accountAddress] },
+      { txn: opt6, signers: [accountAddress] },
+      { txn: opt7, signers: [accountAddress] },
+      { txn: opt8, signers: [accountAddress] },
+      { txn: opt9, signers: [accountAddress] },
     ];
 
     await signAndSend(multipleTxnGroups, algod);
